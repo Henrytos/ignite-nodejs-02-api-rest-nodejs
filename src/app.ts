be6637d0a9849a2d7e1a94051af70260490276d1
Henry <franzhenry46@gmail.com>
2025-02-01 08:54:09 -0300

    chore: update Dockerfile for development and refactor server configuration

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,6 @@ export const app = fastify()
 
 app.register(cookie)
 
-
-
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 })
-
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: 3333, host: '0.0.0.0' }, () => {
-  console.log('Server running on port 3333');
-});
+app.listen({ port: env.PORT, host: env.HOST }, () => {
+  console.log('Server running on port 3333')
+})
